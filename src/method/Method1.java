a3a1d8d3bcff32a228c9bd9a837e8febde2b7639
minseo <alstj778@gmail.com>
2024-03-13 21:30:51 +0900

    240313 method1

---
+++ src/method/Method1.java
@@ -0,0 +1,19 @@
+package method;
+
+public class Method1 {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        System.out.println(a + " + " + b);
+        int sum1 = a+b;
+        System.out.println("result: " + sum1);
+
+        System.out.println();
+
+        int x = 10;
+        int y = 20;
+        System.out.println(x + " + " + y);
+        int sum2 = x+y;
+        System.out.println("result: " + sum2);
+    }
+}
